9aa8b7b0bf8e0f05563499940abb1cef9cde3471
Theelf111 <alvaedvardsson@hotmail.com>
2026-02-13 13:17:03 +0100

    added textMesh and Mesh.setText functions

--- remoteOverlayClient/__init__.py
+++ remoteOverlayClient/__init__.py
@@ -29,51 +29,83 @@ class Mesh(backend.ClientMesh):
             self.sendUpdate(server)
         for name in self.uniforms:
             value = self.uniforms[name]
-            type = str(len(value))+str(type(value))[0]
-
+            count = len(value)
+            valueType = type(value[0])
+            if count in range(1, 5) and valueType in (float, int):
+                Mesh.__getattribute__(self, "sendUniform" + str(count) + ("f" if valueType == float else "i"))(server, name, *value)
+            else:
+                print(f"Uniform \"{name}\" with value {value} has invalid type ({count}, {valueType})")
         server.send()
+
+    def setText(self, pos, text, color0 = (0.0, 0, 0, 1), color1 = (1.0, 1, 1, 1)):
+        self.data = ()
+        x = 0
+        y = 0
+        for i in range(len(text)):
+            if text[i] == "\n":
+                x = 0
+                y += 1
+                continue
+            self.data += (pos[0] + x * 9, pos[1] + y * 20, ord(text[i]) - ord(" ") if ord(text[i]) in range(ord(" "), ord("ÿ") + 1) else ord(" "))
+            x += 1
+        self.uniforms["col0"] = color0
+        self.uniforms["col1"] = color1
 
     def selectShader(self, shader):
         self.shader = shader
         self.sendSelectShader(server, shader)
+        server.send()
 
     def uniformTexture(self, name, texture):
         self.textures[name] = texture
         self.sendUniformTexture(server, name, texture)
-
-    def uniform(self, t, name, *xs):
-        Mesh.__getattribute__("uniform"+t)(self, name, *xs)
+        server.send()
 
     def uniform1f(self, name, *xs):
         self.uniforms[name] = xs
         self.sendUniform1i(server, name, *xs)
+        server.send()
 
     def uniform2f(self, name, *xs):
         self.uniforms[name] = xs
         self.sendUniform1i(server, name, *xs)
+        server.send()
 
     def uniform3f(self, name, *xs):
         self.uniforms[name] = xs
         self.sendUniform1i(server, name, *xs)
+        server.send()
 
     def uniform4f(self, name, *xs):
         self.uniforms[name] = xs
         self.sendUniform1i(server, name, *xs)
+        server.send()
 
     def uniform1i(self, name, *xs):
         self.uniforms[name] = xs
         self.sendUniform1i(server, name, *xs)
+        server.send()
 
     def uniform2i(self, name, *xs):
         self.uniforms[name] = xs
         self.sendUniform1i(server, name, *xs)
+        server.send()
 
     def uniform3i(self, name, *xs):
         self.uniforms[name] = xs
         self.sendUniform1i(server, name, *xs)
+        server.send()
 
     def uniform4i(self, name, *xs):
         self.uniforms[name] = xs
         self.sendUniform1i(server, name, *xs)
+        server.send()
+
+def textMesh(pos, text, color0 = (0.0, 0, 0, 1), color1 = (1.0, 1, 1, 1)):
+    mesh = Mesh("builtins:text", None,
+                {"charSize" : (9.0, 20)},
+                {"tex" : "builtins:font"})
+    mesh.setText(pos, text, color0, color1)
+    return mesh
 
 listen()
